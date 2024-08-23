6ee8adb7cd967611bdff5b5fc10738624be220d8
Jeevarajan <senorjeeva@gmail.com>
2024-08-23 21:43:57 +0530

    Mail service Created

---
+++ src/main/java/com/mrj/ska/service/MailService.java
@@ -0,0 +1,11 @@
+package com.mrj.ska.service;
+
+import jakarta.mail.MessagingException;
+
+public interface MailService {
+	
+	public void sendOtp(String toMail, String otp) throws MessagingException;
+	
+	public String genOtp();
+
+}
